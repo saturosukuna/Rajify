93a9c8f569c2d63d457a06632ff2b98b0275bb10
saturosukuna <rajeshravi7796@gmail.com>
2025-02-24 08:27:03 +0530

    updated things

---
+++ public/sw.js
@@ -0,0 +1,31 @@
+// public/sw.js
+const CACHE_NAME = 'rajify-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/assets/*',
+  // Add other static assets you want to cache
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+    )
+  );
+});
